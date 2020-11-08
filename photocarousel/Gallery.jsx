25bdea63f465d017b17b1fd21e7d170b52de78a7
Tom Nguyen <tommy.nguyen0721@gmail.com>
2020-11-08 11:03:50 -0800

    Clean up gallery.jsx

--- photocarousel/Gallery.jsx
+++ photocarousel/Gallery.jsx
@@ -10,9 +10,6 @@ const Gallery = ({list, openModal, closeModal, show}) => (
     </GalleryWrapper>
     <SubWrapper>
       {list.photos.slice(1, 5).map((photo, index) => {
-        console.log(list)
-        console.log(photo, 'photo')
-        console.log(index, 'index')
         return (<Grid picIndex={index}>
             <Imgagery src={photo} key={index} onClick={(e) => openModal(index + 1)}/>
         </Grid>)
